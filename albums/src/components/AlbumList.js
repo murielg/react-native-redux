cb9e5252a8bc73aa0d10666e165e25c4af27f9be
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-07 09:47:12 -0500

    ListView fix to AlbumList component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -6,22 +6,31 @@ class AlbumList extends Component {
   state = { albums: [] };
 
   componentWillMount() {
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+     dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+     }),
+      loaded: false
     };
 
     fetch('https://itunes.apple.com/search?term=no+doubt&limit=5&entity=album')
     .then((response) => response.json())
       .then((responseData) => {
-        //console.log(responseData.results);
-        //this.setState({ albums: responseData.results});
-        //
-        this.setState({dataSource:ds.cloneWithRow(responseData.results)});
-      });  
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.results),
+          loaded: true
+        });      
+      })
+      .done();  
   }
-  renderAlbums() {
+  renderAlbums(album) {
     return this.state.albums.map(album => 
+      <Text>{album.collectionName}</Text>
+    );
+  }
+
+  renderAlbum(album){
+    return (
       <Text>{album.collectionName}</Text>
     );
   }
@@ -30,9 +39,9 @@ class AlbumList extends Component {
     return (
       <View style={{flex:1}}>
         <ListView
-          dataSource={this.dataSource}
-          renderRow={(rowData) => <Text>{rowData}</Text>}
-        />
+        dataSource={this.state.dataSource}
+        renderRow={this.renderAlbum}
+      />
       </View>
     );
 
