8e972de86066f4b26953b6477129a7de113badeb
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-08 12:59:06 -0500

    fixing eslint

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-
+import { Header, Button, Card, CardSection } from './components/common';
 class App extends Component {
   render() {
     return (
-      <View>
-        <Text>App!</Text>
-      </View>
+      <Text>App</Text>
+
     );
   }
 }
