a532899cbd15c7a9c2d351cdcd04dd1c3dd71afe
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-08 15:49:42 -0500

    adding react dotenv

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header, Button, Card, CardSection } from './components/common';
+import firebase from 'firebase';
+import { FB_API, FB_DOMAIN, FB_DB, FB_ID, FB_SB, FB_MSID } from 'react-native-dotenv';
+import { Header } from './components/common';
+
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: FB_API,
+      authDomain: FB_DOMAIN,
+      databaseURL: FB_DB,
+      projectId: FB_ID,
+      storageBucket: FB_SB,
+      messagingSenderId: FB_MSID
+    });
+  }
   render() {
     return (
-      <Text>App</Text>
-
+      <View>
+        <Header headerText="Authentication" />
+        <Text>App</Text>
+      </View>
     );
   }
 }
 export default App;
-
-
