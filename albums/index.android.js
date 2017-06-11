5e0995471179f543831e44a12faf1a9dcaa71a36
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-11 12:44:22 -0500

    index.android.js

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,14 +1,17 @@
-// index.android.js
-// Android code goes here
-
+// Index.android.js 
+// android code goes here
+// TODO: make app component and remove duplicate code from
+// index ios and index android
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
       <Header headerText={'Albums'} />
+      <AlbumList />
     </View>  
   );
 };
