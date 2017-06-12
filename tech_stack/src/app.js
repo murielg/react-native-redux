fba4db772001f40b7a959df04617b8f545012df4
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-12 16:50:27 -0500

    added common shared components

--- tech_stack/src/app.js
+++ tech_stack/src/app.js
@@ -3,14 +3,16 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)} >
-    <View>
-      <Text>hi</Text>
-    </View>
-  </Provider>
+      <View>
+        <Header headerText="Tech Stack"/>  
+        <Text>yolo</Text>
+      </View>
+    </Provider>
   )
 };
 
