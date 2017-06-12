b3dc433a7aed285692cf00f841ef78cd51a943d0
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-12 16:43:24 -0500

    redux boilerplate

---
+++ tech_stack/src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)} >
+    <View>
+      <Text>hi</Text>
+    </View>
+  </Provider>
+  )
+};
+
+export default App;
