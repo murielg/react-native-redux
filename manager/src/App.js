57f784701b743efc7fcef2c727c8624fc8e9db26
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-22 15:27:39 -0500

    adding common components

--- manager/src/App.js
+++ manager/src/App.js
@@ -5,9 +5,7 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 import firebase from 'firebase';
 import { FB_API, FB_DOMAIN, FB_DB, FB_ID, FB_SB, FB_MSID } from 'react-native-dotenv';
-class App extends Component {
-    
-  
+class App extends Component { 
   componentWillMount() {
      const config = {
       apiKey: `${FB_API}`,
