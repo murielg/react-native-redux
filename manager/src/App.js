47251a7354dcbb19104106128c06920c7f05cd63
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-22 15:18:07 -0500

    more boilerplate stuff

--- manager/src/App.js
+++ manager/src/App.js
@@ -3,8 +3,24 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
-
+import firebase from 'firebase';
+import { FB_API, FB_DOMAIN, FB_DB, FB_ID, FB_SB, FB_MSID } from 'react-native-dotenv';
 class App extends Component {
+    
+  
+  componentWillMount() {
+     const config = {
+      apiKey: `${FB_API}`,
+      authDomain: `${FB_DOMAIN}`,
+      databaseURL: `${FB_DB}`,
+      projectId: `${FB_ID}`,
+      storageBucket: `${FB_SB}`,
+      messagingSenderId: `${FB_MSID}`
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return(
       <Provider store={createStore(reducers)}>
