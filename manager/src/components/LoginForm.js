b15157df282b349b30e45deff91cb847579326ac
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-23 10:59:43 -0500

    wired up redux for login form

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged } from '../actions';
 
 class LoginForm extends Component {
   onEmailChange(text) {
     this.props.emailChanged(text);
+  }
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
   }
   
   render() {
@@ -16,6 +19,7 @@ class LoginForm extends Component {
             label="Email"
             placeholder="email@email.com"
             onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
           />
         </CardSection>
         <CardSection>
@@ -23,6 +27,8 @@ class LoginForm extends Component {
             secureTextEntry
             label="Password"
             placeholder="password"
+            onChangeText={this.onPasswordChange.bind(this)}
+             value={this.props.password}
           />
         </CardSection>
         <CardSection>
@@ -34,4 +40,12 @@ class LoginForm extends Component {
     );
   }
 }
-export default connect(null, {emailChanged})(LoginForm);
+
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email, 
+    password: state.auth.password
+  };
+};
+
+export default connect(mapStateToProps, {emailChanged, passwordChanged})(LoginForm);
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,9 +1,13 @@
-const INITIAL_STATE = { email: '' };
+import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+
+const INITIAL_STATE = { email: '', password: '' };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'email_changed':
-      return;
+    case EMAIL_CHANGED:
+      return { ...state, email: action.payload };
+    case PASSWORD_CHANGED: 
+      return { ...state, password: action.payload};
     default: 
       return state;
   }
