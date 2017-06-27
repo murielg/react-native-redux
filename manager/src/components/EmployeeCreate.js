db7a7cc32c6039bc84bda183a4bfeec63a0bb660
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-06-27 17:03:34 -0500

    adding employee update action and reducer

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Create Employee</Text>
-      </View>
-    
+      <Card>
+        <CardSection>
+          <Input 
+            label="Name"
+            placeholder="Name"
+          />
+        </CardSection>
+        <CardSection>
+          <Input 
+            label="Phone"
+            placeholder="555-555-5555"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+      </CardSection>
+
+      </Card>
+
     );
   }
-  
+
 }
 
 export default EmployeeCreate;
