8f3ed3833768a741f791a2969bde3ee8cc5f335d
Muriel Gonzalez <murielgonzalez@gmail.com>
2017-11-16 11:08:47 -0600

    swipe app

--- swipe/App.js
+++ swipe/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ball from './src/ball'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Ball/>
       </View>
     );
   }
